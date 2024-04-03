9592865325fd68fd8d0d1e649382fb498386454c
Alona7777 <abogolepova@gmail.com>
2024-04-03 15:34:27 -0700

    +

--- src/services/email.py
+++ src/services/email.py
@@ -1,4 +1,4 @@
-# import certify
+import certifi
 from pathlib import Path
 
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
@@ -7,19 +7,35 @@ from pydantic import EmailStr
 
 from src.services.auth import auth_service
 
+# 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
+
+# certificate = certifi.where()
+# conf = ConnectionConfig(
+#     MAIL_USERNAME="contactsbook_fastapi@yahoo.com",
+#     MAIL_PASSWORD="Fast12345678api",
+#     MAIL_FROM="contactsbook_fastapi@yahoo.com",
+#     MAIL_PORT=465,
+#     MAIL_SERVER="smtp.mail.yahoo.com",
+#     MAIL_FROM_NAME="Contact Book Assistant",
+#     MAIL_STARTTLS=False,
+#     MAIL_SSL_TLS=True,
+#     USE_CREDENTIALS=True,
+#     VALIDATE_CERTS=True,   #certifi.where()
+#     TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
+# )
 conf = ConnectionConfig(
-    MAIL_USERNAME="contactsbook_fastapi@yahoo.com",
-    MAIL_PASSWORD="Fast12345678api",
+    MAIL_USERNAME="ContactBook",
+    MAIL_PASSWORD="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",
     MAIL_FROM="contactsbook_fastapi@yahoo.com",
-    MAIL_PORT=465,
-    MAIL_SERVER="smtp.mail.yahoo.com",
-    MAIL_FROM_NAME="Contact Book Assistant",
-    MAIL_STARTTLS=False,
-    MAIL_SSL_TLS=True,
+    MAIL_PORT=587,
+    MAIL_SERVER="smtp.sendgrid.net", 
+    MAIL_TLS=True,
+    MAIL_SSL=False,
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True,
     TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
 )
+
 
 async def send_email(email: EmailStr, username: str, host: str):
     try: 
