c0e3490f521f0ed27c4d92382eeffef23fa5c7be
Alona7777 <abogolepova@gmail.com>
2024-04-04 08:26:08 -0700

    add security

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,3 +1,36 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    sqlalchemy_database_url: str
+    secret_key: str
+    algorithm: str
+    mail_username: str
+    mail_password: str
+    mail_from: str
+    mail_port: int
+    mail_server: str
+    mail_y_username: str
+    mail_y_password: str
+    mail_y_from: str
+    mail_y_port: int
+    mail_y_server: str
+    mail_sg_username: str
+    mail_sg_password: str
+    mail_sg_from: str
+    mail_sg_port: int
+    mail_sg_server: str
+    redis_host: str = 'localhost'
+    redis_port: int = 6379
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
+
+
 class Config:
     DB_URL_ASYNG = "postgresql+asyncpg://postgres:12345@localhost:5432/contacts_app"
     DB_URL = "postgresql+psycopg2://postgres:12345@localhost:5432/contacts_app"
--- src/services/email.py
+++ src/services/email.py
@@ -5,15 +5,15 @@ from fastapi_mail.errors import ConnectionErrors
 from pydantic import EmailStr
 
 from src.services.auth import auth_service
+from src.conf.config import settings
 
 
-# certificate = certifi.where()
 conf = ConnectionConfig(
-    MAIL_USERNAME="fatsapiuser@meta.ua",
-    MAIL_PASSWORD="pythonCourse2023",
-    MAIL_FROM="fatsapiuser@meta.ua",
-    MAIL_PORT=465,
-    MAIL_SERVER="smtp.meta.ua",
+    MAIL_USERNAME=settings.mail_username,
+    MAIL_PASSWORD=settings.mail_password,
+    MAIL_FROM=settings.mail_from,
+    MAIL_PORT=settings.mail_port,
+    MAIL_SERVER=settings.mail_server,
     MAIL_FROM_NAME="TODO Systems",
     MAIL_STARTTLS=False,
     MAIL_SSL_TLS=True,
@@ -22,11 +22,11 @@ conf = ConnectionConfig(
     TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
 )
 # conf = ConnectionConfig(
-#     MAIL_USERNAME="contactsbook_fastapi@yahoo.com",
-#     MAIL_PASSWORD="Fast12345678api",
-#     MAIL_FROM="contactsbook_fastapi@yahoo.com",
-#     MAIL_PORT=465,
-#     MAIL_SERVER="smtp.mail.yahoo.com",
+#     MAIL_USERNAME=settings.mail_y_username,
+#     MAIL_PASSWORD=settings.mail_y_password,
+#     MAIL_FROM=settings.mail_y_from,
+#     MAIL_PORT=settings.mail_y_port,
+#     MAIL_SERVER=settings.mail_y_server,
 #     MAIL_FROM_NAME="Contact Book Assistant",
 #     MAIL_STARTTLS=False,
 #     MAIL_SSL_TLS=True,
@@ -35,11 +35,11 @@ conf = ConnectionConfig(
 #     TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
 # )
 # conf = ConnectionConfig(
-#     MAIL_USERNAME="apikey",
-#     MAIL_PASSWORD=,
-#     MAIL_FROM="contactsbook_fastapi@yahoo.com",
-#     MAIL_PORT=465,
-#     MAIL_SERVER="smtp.sendgrid.net", 
+#     MAIL_USERNAME=settings.mail_sg_username,
+#     MAIL_PASSWORD=settings.mail_sg_password,
+#     MAIL_FROM=settings.mail_sg_from,
+#     MAIL_PORT=settings.mail_sg_port,
+#     MAIL_SERVER=settings.mail_sg_server, 
 #     MAIL_FROM_NAME="Contact Book Assistant",
 #     MAIL_STARTTLS=False,
 #     MAIL_SSL_TLS=True,
