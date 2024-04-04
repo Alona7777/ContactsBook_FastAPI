f82e0783b748cd97dede94e130c45b18aa6e38cf
Alona7777 <abogolepova@gmail.com>
2024-04-03 17:25:57 -0700

    all

--- src/services/email.py
+++ src/services/email.py
@@ -10,6 +10,19 @@ from src.services.auth import auth_service
 # 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
 
 # certificate = certifi.where()
+conf = ConnectionConfig(
+    MAIL_USERNAME="fatsapiuser@meta.ua",
+    MAIL_PASSWORD="pythonCourse2023",
+    MAIL_FROM="fatsapiuser@meta.ua",
+    MAIL_PORT=465,
+    MAIL_SERVER="smtp.meta.ua",
+    MAIL_FROM_NAME="TODO Systems",
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+    TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
+)
 # conf = ConnectionConfig(
 #     MAIL_USERNAME="contactsbook_fastapi@yahoo.com",
 #     MAIL_PASSWORD="Fast12345678api",
@@ -23,18 +36,21 @@ from src.services.auth import auth_service
 #     VALIDATE_CERTS=True,   #certifi.where()
 #     TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
 # )
-conf = ConnectionConfig(
-    MAIL_USERNAME="ContactBook",
-    MAIL_PASSWORD="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",
-    MAIL_FROM="contactsbook_fastapi@yahoo.com",
-    MAIL_PORT=587,
-    MAIL_SERVER="smtp.sendgrid.net", 
-    MAIL_TLS=True,
-    MAIL_SSL=False,
-    USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True,
-    TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
-)
+# conf = ConnectionConfig(
+#     MAIL_USERNAME="apikey",
+#     MAIL_PASSWORD="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",
+#     MAIL_FROM="contactsbook_fastapi@yahoo.com",
+#     MAIL_PORT=465,
+#     MAIL_SERVER="smtp.sendgrid.net", 
+#     MAIL_FROM_NAME="Contact Book Assistant",
+#     MAIL_STARTTLS=False,
+#     MAIL_SSL_TLS=True,
+#     # MAIL_TLS=True,
+#     # MAIL_SSL=False,
+#     USE_CREDENTIALS=True,
+#     VALIDATE_CERTS=True,
+#     TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
+# )
 
 
 async def send_email(email: EmailStr, username: str, host: str):
