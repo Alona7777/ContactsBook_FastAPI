f9f245399faa73be0a2a6ac288d97a89a5cdb8f9
Alona7777 <abogolepova@gmail.com>
2024-04-05 10:01:42 -0700

    fixed email sending

--- src/services/email.py
+++ src/services/email.py
@@ -8,43 +8,15 @@ from src.services.auth import auth_service
 from src.conf.config import settings
 
 
-# conf = ConnectionConfig(
-#     MAIL_USERNAME=settings.mail_username,
-#     MAIL_PASSWORD=settings.mail_password,
-#     MAIL_FROM=settings.mail_from,
-#     MAIL_PORT=settings.mail_port,
-#     MAIL_SERVER=settings.mail_server,
-#     MAIL_FROM_NAME="TODO Systems",
-#     MAIL_STARTTLS=False,
-#     MAIL_SSL_TLS=True,
-#     USE_CREDENTIALS=True,
-#     VALIDATE_CERTS=True,
-#     TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
-# )
-# conf = ConnectionConfig(
-#     MAIL_USERNAME=settings.mail_y_username,
-#     MAIL_PASSWORD=settings.mail_y_password,
-#     MAIL_FROM=settings.mail_y_from,
-#     MAIL_PORT=settings.mail_y_port,
-#     MAIL_SERVER=settings.mail_y_server,
-#     MAIL_FROM_NAME="Contact Book Assistant",
-#     MAIL_STARTTLS=False,
-#     MAIL_SSL_TLS=True,
-#     USE_CREDENTIALS=True,
-#     VALIDATE_CERTS=True, 
-#     TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
-# )
 conf = ConnectionConfig(
-    MAIL_USERNAME=settings.mail_sg_username,
-    MAIL_PASSWORD=settings.mail_sg_password,
-    MAIL_FROM=settings.mail_sg_from,
-    MAIL_PORT=settings.mail_sg_port,
-    MAIL_SERVER=settings.mail_sg_server, 
+    MAIL_USERNAME=settings.mail_username,
+    MAIL_PASSWORD=settings.mail_password,
+    MAIL_FROM=settings.mail_from,
+    MAIL_PORT=settings.mail_port,
+    MAIL_SERVER=settings.mail_server,
     MAIL_FROM_NAME="Contact Book Assistant",
     MAIL_STARTTLS=False,
     MAIL_SSL_TLS=True,
-    # MAIL_TLS=True,
-    # MAIL_SSL=False,
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True,
     TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
